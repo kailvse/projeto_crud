44b705f07a04eee5d7a8e46874ee00e0de24782e
kailvse <kaiozinn4@gmail.com>
2025-03-10 08:43:10 -0300

    Criação da rota /listar

--- backend/produtos/src/main/java/br/com/produtos/crudprodutos/produtos/controle/ProductController.java
+++ backend/produtos/src/main/java/br/com/produtos/crudprodutos/produtos/controle/ProductController.java
@@ -1,13 +1,25 @@
 package br.com.produtos.crudprodutos.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.produtos.crudprodutos.produtos.modelo.ProdutoModelo;
+import br.com.produtos.crudprodutos.produtos.servico.ProductService;
 
 @RestController
 public class ProductController {
 
+  @Autowired
+  private ProductService ps;
+
+  @GetMapping("/listar")
+  public Iterable<ProdutoModelo> listar(){
+    return ps.listar();
+  }
+
   @GetMapping("/")
-  public String route() {
+  public String rota() {
     return "API esta funcionando!";
   }
 }

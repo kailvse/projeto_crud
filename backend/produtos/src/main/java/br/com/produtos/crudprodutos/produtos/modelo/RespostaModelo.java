d92b3748269b7ae81363119c2221aa5fc8645f19
kailvse <kaiozinn4@gmail.com>
2025-03-18 07:37:05 -0300

    Criação do método cadastrar

--- backend/produtos/src/main/java/br/com/produtos/crudprodutos/produtos/modelo/RespostaModelo.java
+++ backend/produtos/src/main/java/br/com/produtos/crudprodutos/produtos/modelo/RespostaModelo.java
@@ -9,5 +9,5 @@ import lombok.Setter;
 @Setter
 @Getter
 public class RespostaModelo {
-    
+    private String resposta;
 }
--- backend/produtos/src/main/java/br/com/produtos/crudprodutos/produtos/servico/ProductService.java
+++ backend/produtos/src/main/java/br/com/produtos/crudprodutos/produtos/servico/ProductService.java
@@ -1,19 +1,39 @@
 package br.com.produtos.crudprodutos.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.produtos.crudprodutos.produtos.repositorio.ProductRepository;
 import br.com.produtos.crudprodutos.produtos.modelo.ProdutoModelo;
+import br.com.produtos.crudprodutos.produtos.modelo.RespostaModelo;
 
 @Service
 public class ProductService {
 
     @Autowired
     private ProductRepository pr;
-
+    
+    @Autowired
+    private RespostaModelo rm;
+    //Listar Produto
     public Iterable<ProdutoModelo> listar(){
         return pr.findAll();
+    }
+
+    //Cadastrar produtos
+    public ResponseEntity<?> cadastrar(ProdutoModelo pm){
+        System.out.println("aqui");
+        if(pm.getName().equals("")){
+            rm.setResposta("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setResposta("O nome da marca do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<ProdutoModelo>(pr.save(pm),HttpStatus.CREATED);
+        }
     }
 
 }

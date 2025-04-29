7d728d11a93fbf6a252ca9bd19b1c9db20c98825
kailvse <kaiozinn4@gmail.com>
2025-04-29 11:12:39 -0300

    .

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
@@ -7,6 +7,13 @@ import Tabela from './Tabela';
 function App() {
 
 const[btnCadastrar] =useState(false);
+const[produtos, setProdutos] =useState ([]);
+
+useEffect (() => {
+  fetch("http://localhost:8080/listar")
+  .then(retorno => retorno.json())
+  .then (retorno_convertido => setProdutos(retorno_convertido))
+},[]);
 
 return (    
       <div>
